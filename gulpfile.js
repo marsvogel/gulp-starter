cc47e0f33e04217a025bb0d8ff95931c31950c4d
Christopher Bischoff <chri.bischoff@gmail.com>
2015-02-06 11:19:35 +0100

    Drop gulp-browserify support (use native instead)
    Signed-off-by: Christopher Bischoff <chri.bischoff@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,39 @@ var sass        = require("gulp-ruby-sass");
 var uglify      = require("gulp-uglify");
 var browserify  = require("gulp-browserify");
 var spritesmith = require('gulp.spritesmith');
+var browserify  = require('browserify');
+var buffer      = require('vinyl-buffer');
 var imagemin    = require('gulp-imagemin');
 var uglify      = require('gulp-uglify');
+var sass        = require('gulp-ruby-sass');
+var source      = require('vinyl-source-stream');
 var sourcemaps  = require('gulp-sourcemaps');
+var getBundleName = function () {
+    var version = require('./package.json').version;
+    var name = require('./package.json').name;
+    return version + '.' + name;
+};
 
-gulp.task('scripts',function(){
-    gulp.src(["src/scripts/*.js"])
-        .pipe(browserify({debug:true}))
-        .pipe(uglify())
-        .pipe(gulp.dest("dist/scripts"));
+gulp.task('scripts', function() {
+
+    var bundler = browserify({
+        entries: ['./src/scripts/app.js'],
+        debug: true
+    });
+
+    var bundle = function() {
+        return bundler
+            .bundle()
+            .pipe(source(getBundleName() + '.js'))
+            .pipe(buffer())
+            .pipe(sourcemaps.init({loadMaps: true}))
+            // Add transformation tasks to the pipeline here.
+            .pipe(uglify())
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest("dist/scripts"));
+    };
+
+    return bundle();
 });
 
 gulp.task('styles', function () {
